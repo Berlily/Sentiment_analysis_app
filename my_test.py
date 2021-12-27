612d522bd9276d09eb270278cc2cacd0ee66a526
Varvara Krauchanka <varvarakrauchanka@gmail.com>
2021-12-27 22:13:45 +0300

    CHUNK_SIZE changed back to 1024 in my_test.py

--- my_test.py
+++ my_test.py
@@ -10,7 +10,7 @@ import plot_data
 from utils import extract_feature
 
 THRESHOLD = 500
-CHUNK_SIZE = 560
+CHUNK_SIZE = 1240
 FORMAT = pyaudio.paInt16  # Sound is stored in binary, as is everything related to computers. In order to know where an integer starts and ends, there are different methods used. PyAudio uses a fixed size of bits.
 # paInt16 is basically a signed 16-bit binary string.
 RATE = 16000
